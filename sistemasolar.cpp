57016824269307e5e34b2fe7505164a83af15b7a
MinervaBF <126017587+MinervaBF@users.noreply.github.com>
2023-03-27 19:19:05 +0200

    Update sistemasolar.cpp

--- sistemasolar.cpp
+++ sistemasolar.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+
 #include <math.h>
 #include <fstream>
 
@@ -22,7 +22,7 @@ int main (void)
 
 
     //Abrimos los archivos
-    f1=fopen("condicionesiniciales.txt","r");
+    f1=fopen("condicionesiniciales2.txt","r");
     f2=fopen("posiciontiempoplanetas.txt","w");
 
     //Inicializamos los vectores a 0
@@ -39,14 +39,17 @@ int main (void)
     //Damos valores de condiciones iniciales: masa, posición y velocidad de
     //cada planteta
 
+    printf("Las condiciones iniciales son.\n");
+    printf("i         masa            r_x           r_y              v_x             v_y     \n");
 
     for(i=0; i<=8;i++)
     {
         fscanf(f1,"%lf\t%lf\t%lf",&x,&y,&z);
         //Leemos los datos del archivo reescalando
-        m[i]=(1./1989000000000000000000000000000.)*x;
-        r[0][i]=y/146600000000. ;
-        v[1][i]=(0.00003357193)*z;
+        m[i]=(1./1990000.)*x;
+        r[0][i]=y/149.6;
+        v[1][i]=0.03356227281*z;
+        printf("%i\t%e\t%e\t%e\t%e\t%e\n", i, m[i], r[0][i], r[1][i], v[0][i], v[1][i]);
     }
 
 
@@ -54,11 +57,11 @@ int main (void)
 
     printf("Comprobamos el valor de las aceleraciones.\n");
 
-    for (i=1; i<=8; i++)
+    for (i=0; i<=8; i++)
     {
         for(j=0; j<=8; j++)
         {
-                if (j!=i)
+                if (i!=j)
                 {
                     denom=pow((pow((r[0][i]-r[0][j]),2)+pow((r[1][i]-r[1][j]),2)),0.5);
                     a[0][i]=a[0][i]-m[j]*(r[0][i]-r[0][j])*((pow(denom, -3)));
@@ -66,11 +69,11 @@ int main (void)
                 }
 
             }
-        printf("%lf\t%lf\n", a[0][i],a[1][i]);
+        printf("%e\t%e\n", a[0][i],a[1][i]);
     }
 
 
-h=0.02;
+h=0.01;
 e=0;
 
 
@@ -80,31 +83,38 @@ e=0;
     //Calculamos r(t+h)
     for(i=0; i<=8; i++)
     {
+        //Escribimos los datos en un fichero, separados por una coma
+        fprintf(f2, "%e%c\t%e\n", r[0][i], 44, r[1][i]); //, v[0][i], v[1][i], a[0][i], a[1][0]);
         for(k=0; k<=1; k++)
         {
             r[k][i]=r[k][i]+h*v[k][i]+((h*h)*0.5)*a[k][i];
+            // Fuerzo que el Sol no se mueva
+            r[k][0]=0.;
         }
-        //Escribimos los datos en un fichero, separados por una coma
-        fprintf(f2, "%lf%c\t%lf\t%lf\t%lf\t%lf\t%lf\n", r[0][i], 44, r[1][i]), v[0][i],v[1][i], a[0][i], a[1][i];
-
-
     }
     fprintf(f2, "\n"); //Los datos tienen que estar separados en bloques
 
-    //Calculamos a2(t+h)
+
+
+    //Calculamos aaux(t+h)
     for (i=0; i<=8; i++)
     {
+        // Inicializamos a 0 en cada iteración
+        aaux[0][i]=0.;
+        aaux[1][i]=0.;
         for(j=0; j<=8; j++)
         {
             for(k=0; k<=1; k++)
-                {
-                    if (j!=i)
+            {
+                    if (i!=j)
                     {
-                        denom=((r[0][i]-r[0][j])*(r[0][i]-r[0][j])+(r[1][i]-r[1][j])*(r[1][i]-r[1][j]));
-                        aaux[k][i]=aaux[k][i]-m[j]*(r[k][i]-r[k][j])*((pow(denom, -1.5)));
+                        denom=pow((pow((r[0][i]-r[0][j]),2.)+pow((r[1][i]-r[1][j]),2.)),0.5);
+                        aaux[k][i]=aaux[k][i]-m[j]*(r[k][i]-r[k][j])*((pow(denom, -3.)));
                     }
-                }
+            }
+
         }
+        //printf("%lf\t%lf\n", aaux[0][i], aaux[1][i]);
     }
 
     //Calculamos v(t+h)
@@ -120,11 +130,10 @@ e=0;
 
     e=e+1;
     }
-    while (e<25000);
+    while (e<20000);
 
     fclose(f1);
     fclose(f2);
 
     return 0;
 }
-
